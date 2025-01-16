ff9bc33e0447a5c1708b1f13bb7f95978af4c137
Student <146102832+Student162009@users.noreply.github.com>
2025-01-16 18:37:31 +0300

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -1,306 +1,346 @@
-const fs = require('fs');
-const path = require('path');
-let LOG = ''; 
-let Id = '';
-const userTasksDir = path.join(__dirname, 'json/userTasks');
+const fs = require("fs");
+const path = require("path");
+let LOG = "";
+let Id = "";
+const userTasksDir = path.join(__dirname, "json/userTasks");
 
-const MultiTasksDir = path.join(__dirname, 'json/MultiTasks');
+const MultiTasksDir = path.join(__dirname, "json/MultiTasks");
 
 fs.rmSync(MultiTasksDir, { recursive: true, force: true });
 
 if (!fs.existsSync(userTasksDir)) {
-    fs.mkdirSync(userTasksDir);
+  fs.mkdirSync(userTasksDir);
 }
 
 let Data = [];
 let UserData = [];
 
 const setUserLog = (login) => {
-    Data = [];
-    LOG = login;
+  Data = [];
+  LOG = login;
 };
 
 const setMultiID = (login) => {
-    Data = [];
-    Id = login;
+  Data = [];
+  Id = login;
 };
 
-const UserLOGFilePath = path.join(__dirname, 'json/dataAUT.json');
+const UserLOGFilePath = path.join(__dirname, "json/dataAUT.json");
 
 const writeUserData = (DATA) => {
-    fs.writeFileSync(UserLOGFilePath, JSON.stringify(DATA, null, 2));
+  fs.writeFileSync(UserLOGFilePath, JSON.stringify(DATA, null, 2));
 };
 
 const readUserData = () => {
-    if (UserData.length === 0) {
-        if (!fs.existsSync(UserLOGFilePath) || fs.readFileSync(UserLOGFilePath).length === 0) {
-            UserData = [];
-        } else {
-            try {
-                const rawData = fs.readFileSync(UserLOGFilePath);
-                UserData = JSON.parse(rawData);
-            } catch (error) {
-                console.error("Ошибка при чтении данных:", error);
-                UserData = [];
-            }
-        }
+  if (UserData.length === 0) {
+    if (
+      !fs.existsSync(UserLOGFilePath) ||
+      fs.readFileSync(UserLOGFilePath).length === 0
+    ) {
+      UserData = [];
+    } else {
+      try {
+        const rawData = fs.readFileSync(UserLOGFilePath);
+        UserData = JSON.parse(rawData);
+      } catch (error) {
+        console.error("Ошибка при чтении данных:", error);
+        UserData = [];
+      }
     }
-    return UserData;
+  }
+  return UserData;
 };
 readUserData();
 
 const writeData = (DATA) => {
-    if(Id==''){
-    fs.writeFileSync(path.join(__dirname, `json/userTasks/${LOG}.json`), JSON.stringify(DATA, null, 2));
-    }else{
-    fs.writeFileSync(path.join(__dirname, `json/MultiTasks/${Id}.json`), JSON.stringify(DATA, null, 2));
-    }
+  if (Id == "") {
+    fs.writeFileSync(
+      path.join(__dirname, `json/userTasks/${LOG}.json`),
+      JSON.stringify(DATA, null, 2)
+    );
+  } else {
+    fs.writeFileSync(
+      path.join(__dirname, `json/MultiTasks/${Id}.json`),
+      JSON.stringify(DATA, null, 2)
+    );
+  }
 };
 
 const readData = () => {
-    if(Id==''){
+  if (Id == "") {
     if (Data.length === 0) {
-        if (!fs.existsSync(path.join(__dirname, `json/userTasks/${LOG}.json`)) || fs.readFileSync(path.join(__dirname, `json/userTasks/${LOG}.json`)).length === 0) {
-            Data = [];
-        } else {
-            try {
-                const rawData = fs.readFileSync(path.join(__dirname, `json/userTasks/${LOG}.json`));
-                Data = JSON.parse(rawData);
-                console.log(Data);
-            } catch (error) {
-                console.error("Ошибка при чтении данных:", error);
-                Data = [];
-            }
+      if (
+        !fs.existsSync(path.join(__dirname, `json/userTasks/${LOG}.json`)) ||
+        fs.readFileSync(path.join(__dirname, `json/userTasks/${LOG}.json`))
+          .length === 0
+      ) {
+        Data = [];
+      } else {
+        try {
+          const rawData = fs.readFileSync(
+            path.join(__dirname, `json/userTasks/${LOG}.json`)
+          );
+          Data = JSON.parse(rawData);
+          console.log(Data);
+        } catch (error) {
+          console.error("Ошибка при чтении данных:", error);
+          Data = [];
         }
+      }
     }
-}else{
+  } else {
     if (Data.length === 0) {
-        if (!fs.existsSync(path.join(__dirname, `json/MultiTasks/${Id}.json`)) || fs.readFileSync(path.join(__dirname, `json/MultiTasks/${Id}.json`)).length === 0) {
-            Data = [];
-        } else {
-            try {
-                const rawData = fs.readFileSync(path.join(__dirname, `json/MultiTasks/${Id}.json`));
-                Data = JSON.parse(rawData);
-                console.log(Data);
-            } catch (error) {
-                console.error("Ошибка при чтении данных:", error);
-                Data = [];
-            }
+      if (
+        !fs.existsSync(path.join(__dirname, `json/MultiTasks/${Id}.json`)) ||
+        fs.readFileSync(path.join(__dirname, `json/MultiTasks/${Id}.json`))
+          .length === 0
+      ) {
+        Data = [];
+      } else {
+        try {
+          const rawData = fs.readFileSync(
+            path.join(__dirname, `json/MultiTasks/${Id}.json`)
+          );
+          Data = JSON.parse(rawData);
+          console.log(Data);
+        } catch (error) {
+          console.error("Ошибка при чтении данных:", error);
+          Data = [];
         }
+      }
     }
-}
-    return Data;
+  }
+  return Data;
 };
 
-
-const addUserData = ( login, parol, numtel, firstname, name, email) => {
-    const User = UserData.find(user => user.login === login);
-    if (User) {
-        return null; 
-    }
-    const newData = {login, parol, numtel, firstname, name, email};
-    UserData.push(newData);
-    writeUserData(UserData);
-    if(login!=='Admin'){
+const addUserData = (login, parol, numtel, firstname, name, email) => {
+  const User = UserData.find((user) => user.login === login);
+  if (User) {
+    return null;
+  }
+  const newData = { login, parol, numtel, firstname, name, email };
+  UserData.push(newData);
+  writeUserData(UserData);
+  if (login !== "Admin") {
     addUserTaskFile(login);
     setUserLog(login);
     readData();
-}
-    return newData;
-}
-    
-const editPassword = (Login, NewPassword) =>{
-        const LoginIndex = UserData.findIndex(l => l.login === Login);
-        if (LoginIndex === -1) {
-            return null;
-        }
-        UserData[LoginIndex].parol=NewPassword;
-        writeUserData(UserData);
-        return UserData[LoginIndex];
-    
+  }
+  return newData;
 };
 
-const editLogin = (Login, NewLogin) =>{
-    const LoginIndex = UserData.findIndex(l => l.login === Login);
+const editPassword = (Login, Data, NewPassword) => {
+  if (Data.includes("+375")) {
+    const LoginIndex = UserData.findIndex(
+      (l) => l.login === Login && l.numtel === Data
+    );
     if (LoginIndex === -1) {
-        return null;
+      return null;
     }
-    const oldFilePath = path.join(userTasksDir, `${LOG}.json`);
-    const newFilePath = path.join(userTasksDir, `${NewLogin}.json`);
-
-    UserData[LoginIndex].login = NewLogin;
-    fs.renameSync(oldFilePath, newFilePath);
-    setUserLog(NewLogin);
-    readData();
+    UserData[LoginIndex].parol = NewPassword;
     writeUserData(UserData);
     return UserData[LoginIndex];
-
-};
-
-const editFI = (Login, NewName, NewFirstName) =>{
-    const LoginIndex = UserData.findIndex(l => l.login === Login);
+  } else if (Data.includes("@")) {
+    const LoginIndex = UserData.findIndex(
+      (l) => l.login === Login && l.email === Data
+    );
     if (LoginIndex === -1) {
-        return null;
+      return null;
     }
-    UserData[LoginIndex].name = NewName;
-    UserData[LoginIndex].firstname = NewFirstName;
+    UserData[LoginIndex].parol = NewPassword;
     writeUserData(UserData);
     return UserData[LoginIndex];
-
+  }
 };
 
-const deleteUserData = (Login) => {
-    const userIndex = UserData.findIndex(l => l.login === Login);
-    if (userIndex === -1) {
-        return false; 
-    }
-    UserData.splice(userIndex, 1);
-    const FilePath = path.join(userTasksDir, `${Login}.json`);
-    fs.unlinkSync(FilePath);
-    writeUserData(UserData);
-    return true;
+const editLogin = (Login, password, NewLogin) => {
+  const LoginIndex = UserData.findIndex(
+    (l) => l.login === Login && l.parol === password
+  );
+  if (LoginIndex === -1) {
+    return null;
+  }
+  const oldFilePath = path.join(userTasksDir, `${LOG}.json`);
+  const newFilePath = path.join(userTasksDir, `${NewLogin}.json`);
+
+  UserData[LoginIndex].login = NewLogin;
+  fs.renameSync(oldFilePath, newFilePath);
+  setUserLog(NewLogin);
+  readData();
+  writeUserData(UserData);
+  return UserData[LoginIndex];
+};
+
+const editFI = (Login, password, NewName, NewFirstName) => {
+  const LoginIndex = UserData.findIndex(
+    (l) => l.login === Login && l.parol === password
+  );
+  if (LoginIndex === -1) {
+    return null;
+  }
+  UserData[LoginIndex].name = NewName;
+  UserData[LoginIndex].firstname = NewFirstName;
+  writeUserData(UserData);
+  return UserData[LoginIndex];
+};
+
+const deleteUserData = (Login, password) => {
+  const userIndex = UserData.findIndex(
+    (l) => l.login === Login && l.parol === password
+  );
+  if (userIndex === -1) {
+    return false;
+  }
+  UserData.splice(userIndex, 1);
+  const FilePath = path.join(userTasksDir, `${Login}.json`);
+  fs.unlinkSync(FilePath);
+  writeUserData(UserData);
+  return true;
 };
 
 const addUserTaskFile = (login) => {
-    const taskFilePath = path.join(userTasksDir, `${login}.json`);
-    if (!fs.existsSync(taskFilePath)) {
-        fs.writeFileSync(taskFilePath, JSON.stringify([])); 
-    }
+  const taskFilePath = path.join(userTasksDir, `${login}.json`);
+  if (!fs.existsSync(taskFilePath)) {
+    fs.writeFileSync(taskFilePath, JSON.stringify([]));
+  }
 };
 
 const addMultiTaskFile = (login) => {
-    if (!fs.existsSync(MultiTasksDir)) {
-        fs.mkdirSync(MultiTasksDir);
-    }
-    const taskFilePath = path.join(MultiTasksDir, `${login}.json`);
-    if (!fs.existsSync(taskFilePath)) {
-        fs.writeFileSync(taskFilePath, JSON.stringify([])); 
-    }
+  if (!fs.existsSync(MultiTasksDir)) {
+    fs.mkdirSync(MultiTasksDir);
+  }
+  const taskFilePath = path.join(MultiTasksDir, `${login}.json`);
+  if (!fs.existsSync(taskFilePath)) {
+    fs.writeFileSync(taskFilePath, JSON.stringify([]));
+  }
 };
 
 const addData = (task, category) => {
-    const newData = { task, category }; 
-    Data.push(newData);
-    writeData(Data);
-    return newData;
+  const newData = { task, category };
+  Data.push(newData);
+  writeData(Data);
+  return newData;
 };
 
 const GetID = () => {
-    return Id;
+  return Id;
 };
 
 const AddID = () => {
-    const id  = Math.floor(1000000000000000 + Math.random() * 9000000000000000);
-    setMultiID(id);
-    return id;
+  const id = Math.floor(1000000000000000 + Math.random() * 9000000000000000);
+  setMultiID(id);
+  return id;
 };
 
 const editData = (task, newTaskData) => {
-    const taskIndex = Data.findIndex(t => t.task === task);
-    if (taskIndex === -1) {
-        return null;
-    }
-    Data[taskIndex].task = newTaskData;
-    writeData(Data);
-    return Data[taskIndex];
+  const taskIndex = Data.findIndex((t) => t.task === task);
+  if (taskIndex === -1) {
+    return null;
+  }
+  Data[taskIndex].task = newTaskData;
+  writeData(Data);
+  return Data[taskIndex];
 };
-
 
 const deleteData = (task) => {
-    const taskIndex = Data.findIndex(t => t.task === task);
-    if (taskIndex === -1) {
-        return false; 
-    }
-    Data.splice(taskIndex, 1);
-    writeData(Data);
-    return true;
+  const taskIndex = Data.findIndex((t) => t.task === task);
+  if (taskIndex === -1) {
+    return false;
+  }
+  Data.splice(taskIndex, 1);
+  writeData(Data);
+  return true;
 };
 const sortTasks = () => {
-    return Data.sort((a, b) => a.task.localeCompare(b.task));
+  return Data.sort((a, b) => a.task.localeCompare(b.task));
 };
 
 const markTaskComplete = (task, category) => {
-    const taskIndex = Data.findIndex(t => t.task === task && t.category === category);
-    if (taskIndex === -1) {
-        return null;
-    }
-    Data[taskIndex].completed = true;
-    writeData(Data);
-    return Data[taskIndex];
+  const taskIndex = Data.findIndex(
+    (t) => t.task === task && t.category === category
+  );
+  if (taskIndex === -1) {
+    return null;
+  }
+  Data[taskIndex].completed = true;
+  writeData(Data);
+  return Data[taskIndex];
 };
 const addTaskWithDeadline = (task, deadline, category) => {
-    const newData = { task, deadline, completed: false, category };
-    Data.push(newData);
-    writeData(Data);
-    return newData;
+  const newData = { task, deadline, completed: false, category };
+  Data.push(newData);
+  writeData(Data);
+  return newData;
 };
 
 const checkOverdueTasks = () => {
-    const currentDate = new Date();
-    Data.forEach(task => {
-        if (new Date(task.deadline) < currentDate) {
-            task.overdue = true;
-        }
-    });
-    writeData(Data);
+  const currentDate = new Date();
+  Data.forEach((task) => {
+    if (new Date(task.deadline) < currentDate) {
+      task.overdue = true;
+    }
+  });
+  writeData(Data);
 };
 
 const FindbyCategory = (category) => {
-    const result = Data.filter(c => c.category === category);
-    return result;
+  const result = Data.filter((c) => c.category === category);
+  return result;
 };
 
-const findName = (name) =>{
-    const result =  Data.filter(n => n.task.includes(name))
-    return result;
+const findName = (name) => {
+  const result = Data.filter((n) => n.task.includes(name));
+  return result;
 };
 
 const findDead = (dead) => {
-    const result = Data.filter(d => d.deadline === dead);
-    return result;
+  const result = Data.filter((d) => d.deadline === dead);
+  return result;
 };
 
 const getDead = () => {
-    const currentDate = new Date();
-    const result = Data.filter(task => task.deadline && new Date(task.deadline) < currentDate);
-    return result;
+  const currentDate = new Date();
+  const result = Data.filter(
+    (task) => task.deadline && new Date(task.deadline) < currentDate
+  );
+  return result;
 };
 
 const getCOM = () => {
-    const result = Data.filter(task => task.completed === true);
-    return result;
+  const result = Data.filter((task) => task.completed === true);
+  return result;
 };
 
 const getNotCOM = () => {
-    const result = Data.filter(task => task.completed === false);
-    return result;
+  const result = Data.filter((task) => task.completed === false);
+  return result;
 };
 readData();
 module.exports = {
-     GetUserData: () => [...UserData],
-     GetData: () => [...Data], 
-     addUserData,   
-     addData, 
-     editData, 
-     editPassword,
-     editLogin,
-     editFI,
-     deleteData, 
-     deleteUserData,
-     sortTasks, 
-     addTaskWithDeadline, 
-     markTaskComplete, 
-     FindbyCategory,
-     findName,
-     findDead,
-     getDead,  
-     getCOM,
-     getNotCOM,
-     setUserLog,
-     readData,
-     GetID,
-     AddID,
-     addUserTaskFile,
-     addMultiTaskFile,
-     setMultiID
-    };
+  GetUserData: () => [...UserData],
+  GetData: () => [...Data],
+  addUserData,
+  addData,
+  editData,
+  editPassword,
+  editLogin,
+  editFI,
+  deleteData,
+  deleteUserData,
+  sortTasks,
+  addTaskWithDeadline,
+  markTaskComplete,
+  FindbyCategory,
+  findName,
+  findDead,
+  getDead,
+  getCOM,
+  getNotCOM,
+  setUserLog,
+  readData,
+  GetID,
+  AddID,
+  addUserTaskFile,
+  addMultiTaskFile,
+  setMultiID,
+};
